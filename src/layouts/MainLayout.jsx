5bb998432fb2a2675b928854514a56150a266564
jay-avaiya <jay@pruthatek.com>
2025-12-11 16:48:46 +0530

    Update: Layout Fix

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./main_component/Navbar";
 import Footer from "./main_component/Footer";
 import Blog from "./main_component/Blog";
 import Questions from "./main_component/Questions";
 
 export default function MainLayout() {
+  const location = useLocation();
+  const isBlog = location.pathname.startsWith("/blog");
+
   return (
-    <div className="flex flex-col gap-20">
+    <div className="flex flex-col gap-16">
       <Navbar />
       <Outlet />
-      <Blog />
+      {!isBlog && <Blog />}
       <Questions />
       <Footer />
     </div>
